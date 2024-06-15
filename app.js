32c066d69a176e29f898427cab28b55700e3b9d0
vasa <vasa@fedora.tail3d82ee.ts.net>
2024-06-15 20:25:45 +0530

    changed env var for auth

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-const secret = process.env.secret;
+const secret = process.env.SECRET;
 //session
 app.use(session(
     {
