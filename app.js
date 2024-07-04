18cc7c56a62aff10ec52dcb2c2c81c383af4e964
vasa <vasa@fedora.tail3d82ee.ts.net>
2024-07-04 09:37:33 +0530

    emailid of the user is added to the db

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
 var findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const secret = process.env.SECRET;
 //session
-app.use(session(
-    {
-      secret:secret,
-      resave:false,
-      saveUninitialized:false
-    }
-  ));
-  //passport
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 const uri = process.env.DB;
-mongoose.connect(uri)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB:', err));
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 // Mongoose Schema for Questions and Answers
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-   googleId: String,
-  gmail:String	
-  });
-  //passport-mongoose-local
+  username: String,
+  password: String,
+  googleId: String,
+  gmail: String,
+});
+//passport-mongoose-local
 UserSchema.plugin(passportLocalMongoose);
 UserSchema.plugin(findOrCreate);
 
 //model
 const User = mongoose.model("User", UserSchema);
 const qaSchema = new mongoose.Schema({
-    question: String,
-    answers: [{ type: String }],
-     createdAt: { type: Date, default: Date.now }
+  question: String,
+  answers: [{ type: String }],
+  createdAt: { type: Date, default: Date.now },
 });
- 
+
 const QAColl = mongoose.model("QAColl", qaSchema);
 
 //create strategy using passport from mongoose
@@ -79,34 +76,40 @@ passport.use(
       callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
-    function (accessToken, refreshToken, profile, cb) {
+    function (accessToken, refreshToken, profile, email, cb) {
       console.log(profile);
-      User.findOrCreate({ googleId: profile.id,gmail:profile.email}, function (err, user) {
-        return cb(err, user);
-      });
+      // console.log(email);
+      console.log(email.emails[0].value);
+      User.findOrCreate(
+        { googleId: profile.id, gmail: email.emails[0].value },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
     }
   )
 );
 
 //for login and all
-app.get("/register",async (req,res)=>{
-    res.render('register');
-   
-})
-app.get('/login',async(req,res)=>{
-    res.render("login");
-})
+app.get("/register", async (req, res) => {
+  res.render("register");
+});
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
 
-app.get('/logout', function(req, res, next){
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect('/login');
-    });
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
   });
+});
 
 app.get(
   "/auth/google",
-  passport.authenticate("google", { scope: ["profile"] })
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 app.get(
@@ -116,118 +119,105 @@ app.get(
     // Successful authentication, redirect home.
     // console.log(profile);
     res.redirect("/");
-  })
+  }
+);
 
 // Route to display list of questions (homepage)
 app.get("/", async function (req, res) {
-    if(req.isAuthenticated()){
-        const page = req.query.page?parseInt(req.query.page):1;
+  if (req.isAuthenticated()) {
+    const page = req.query.page ? parseInt(req.query.page) : 1;
     const perpage = 10;
-    const skip = (page - 1)*perpage;
-    const questions = await QAColl.find().sort({ createdAt: -1 }).skip(skip).limit(perpage);
+    const skip = (page - 1) * perpage;
+    const questions = await QAColl.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(perpage);
     const totalRecord = await QAColl.countDocuments();
-    const totalPages = Math.ceil(totalRecord/perpage)
-    res.render("questions", { questions: questions,totalPages,currentPage:page });
-      }
-      else{
-        res.redirect("/login");
-      }
-    
+    const totalPages = Math.ceil(totalRecord / perpage);
+    res.render("questions", {
+      questions: questions,
+      totalPages,
+      currentPage: page,
+    });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // Route to display individual question page
 app.get("/questions/:id", async function (req, res) {
-    if(req.isAuthenticated()){
-        const questionId = req.params.id;
+  if (req.isAuthenticated()) {
+    const questionId = req.params.id;
     const question = await QAColl.findById(questionId);
- const limitedAnswers = question.answers.slice(0,6);
-    res.render("answers", { question: question,answers:limitedAnswers });
-      }
-      else{
-        res.redirect("/login");
-      }
-   
+    const limitedAnswers = question.answers.slice(0, 6);
+    res.render("answers", { question: question, answers: limitedAnswers });
+  } else {
+    res.redirect("/login");
+  }
 });
-
 
 // Route to render form for posting new question
 app.get("/ask", function (req, res) {
-    if(req.isAuthenticated()){
-        res.render("ask");
-      }
-      else{
-        res.redirect("/login");
-      }
-   
+  if (req.isAuthenticated()) {
+    res.render("ask");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // Route to handle posting new question
 app.post("/ask", async function (req, res) {
-    if(req.isAuthenticated()){
-        const newQuestion = new QAColl({
-            question: req.body.question,
-            answers: []
-        });
-        await newQuestion.save();
-        res.redirect("/"); // Redirect to the home page
-      }
-      else{
-        res.redirect("/login");
-      }
-   
+  if (req.isAuthenticated()) {
+    const newQuestion = new QAColl({
+      question: req.body.question,
+      answers: [],
+    });
+    await newQuestion.save();
+    res.redirect("/"); // Redirect to the home page
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // Route to handle posting new answer to a question
 app.post("/questions/:id/answer", async function (req, res) {
-    if(req.isAuthenticated()){
-        const questionId = req.params.id;
-        const answer = req.body.answer;
-        await QAColl.findByIdAndUpdate(questionId, { $push: { answers: answer } });
-        res.redirect("/questions/" + questionId);
-      }
-      else{
-        res.redirect("/login");
-      }
- 
-    
+  if (req.isAuthenticated()) {
+    const questionId = req.params.id;
+    const answer = req.body.answer;
+    await QAColl.findByIdAndUpdate(questionId, { $push: { answers: answer } });
+    res.redirect("/questions/" + questionId);
+  } else {
+    res.redirect("/login");
+  }
 });
 app.post("/register", async (req, res) => {
-    try{
-      await User.register({username:req.body.username},req.body.password);
-      passport.authenticate("local")(req,res,()=>{
-        res.redirect("/");
-      })
-    }
-    catch(err){
-      console.log(err);
-      res.redirect("/register");
-    }
-  
-    
-  
+  try {
+    await User.register({ username: req.body.username }, req.body.password);
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/");
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+});
+app.post("/login", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
-  app.post("/login", async (req, res) => {
-  
-   const user = new User({
-    username:req.body.username,
-    password:req.body.password
-  
-   });
-   req.logIn(user,(err)=>{
-    if(err){
+  req.logIn(user, (err) => {
+    if (err) {
       console.log(err);
       res.redirect("/login");
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/");
+      });
     }
-    else{
-    passport.authenticate('local')(req,res,()=>{
-      res.redirect("/");
-    })
-    }
-   })
-  
   });
-  
+});
 
 app.listen(port, function () {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
